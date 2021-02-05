375c4b64ad243eb8a2bd78260b00b716faacfcd3
jnsjknn <joonas@joonasjokinen.fi>
2021-02-05 11:29:37 +0200

    Use catch-all route

--- server/server.js
+++ server/server.js
@@ -42,4 +42,12 @@ if (['production', 'test'].includes(process.env.NODE_ENV)) {
   );
 }
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
